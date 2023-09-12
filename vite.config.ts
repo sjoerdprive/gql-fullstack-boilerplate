c93f7470e9390b668a7a1ef23dab5a6b84cf7288
Sjoerd Willemsen | Kolibri <sjoerd@kolibri.software>
2023-09-12 13:03:03 +0200

    lightningcss

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  css: {
+    transformer: "lightningcss",
+    lightningcss: {
+      drafts: {
+        nesting: true,
+      },
+    },
+  },
 });
